e13a29f0ff85ba1baf08658d1d3cdb6b07f0983a
Joel <Joel@host.docker.internal>
2025-09-13 21:22:53 +0530

    multi added

--- src/main/java/mypkg/myCalc.java
+++ src/main/java/mypkg/myCalc.java
@@ -3,6 +3,7 @@ package mypkg;
 public class myCalc {
 	int sum(int a, int b) { return a+b; }
 	int diff(int a, int b) { return a-b; }
+    int mul(int a, int b) { return a*b; }
 	double div(int a, int b) { return a/b; }
 
 	public static void main(String[] args) {
@@ -10,6 +11,7 @@ public class myCalc {
 		myCalc calc = new myCalc();
 		System.out.println(" sum a = 40, b = 20  "+ calc.sum(40,20));
 		System.out.println(" diff  a = 40, b = 20  "+ calc.diff(40,20));
+		System.out.println(" multi  a = 40, b = 20  "+ calc.mul(40,20));
 		System.out.println(" div  a = 40, b = 20  "+ calc.div(40,20));
 
 	}
