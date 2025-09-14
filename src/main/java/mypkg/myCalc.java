1b929d21b1337f389be15ef771fe7ff29df45bb0
Joel <Joel@host.docker.internal>
2025-09-14 16:32:55 +0530

    master branch

--- src/main/java/mypkg/myCalc.java
+++ src/main/java/mypkg/myCalc.java
@@ -6,6 +6,10 @@ public class myCalc {
     int mul(int a, int b) { return a*b; }
 	double div(int a, int b) { return a/b; }
 	int mod(int a, int b) { return a%b; }
+	public void branch() 
+	{  
+		System.out.println(" Hello from master branch ");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -15,7 +19,7 @@ public class myCalc {
 		System.out.println(" multi  a = 40, b = 20  "+ calc.mul(40,20));
 		System.out.println(" div  a = 40, b = 20  "+ calc.div(40,20));
 		System.out.println(" mod  a = 40, b = 20  "+ calc.mod(40,20));
-
+		calc.branch();
 	}
 
 }
