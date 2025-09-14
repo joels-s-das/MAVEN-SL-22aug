82de161f541a226d42b82fd0caff25c1ea29364c
Joel <Joel@host.docker.internal>
2025-09-14 15:26:06 +0530

    test

--- src/main/java/mypkg/myCalc.java
+++ src/main/java/mypkg/myCalc.java
@@ -1,7 +1,7 @@
 package mypkg;
 
 public class myCalc {
-	int sum(int a, int b) { return a+b; }
+	int sum(int a, int b) { return a*b; }
 	int diff(int a, int b) { return a-b; }
     int mul(int a, int b) { return a*b; }
 	double div(int a, int b) { return a/b; }
